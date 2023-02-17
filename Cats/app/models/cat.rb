aace5ff918dc6172ae61f0e6ef44097242586169
Kaiter Wu <wukaiter@gmail.com>
2023-02-17 17:50:29 -0500

    adding birthday

---
+++ Cats/app/models/cat.rb
@@ -0,0 +1,36 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+require "action_view"
+include ActionView::Helpers::DateHelper
+
+class Cat < ApplicationRecord
+
+    CAT_COLORS = ["black"]
+    VALID_SEXES = ["M","F"]
+    validates :birth_date, :color, :name, :sex, presence: true 
+    validates :color, inclusion: { in: CAT_COLORS }
+    validates :sex, inclusion: { in: VALID_SEXES }
+    validate :birth_date_cannot_be_future
+
+
+    def birth_date_cannot_be_future
+        errors.add(:birth_date, "can't be in the future") if birth_date > Date.today
+     end 
+
+     def age 
+        time_ago_in_words(self.birth_date)
+     end 
+
+end 
--- Cats/db/seeds.rb
+++ Cats/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+cat = Cat.new(birth_date: Date.today -1, color: "black", name: "millyrock",sex: ":F", description: "angry cat that doesn't know when to stop talking")
